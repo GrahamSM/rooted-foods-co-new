1560ac6ed147f3cff143f41127e7d6e715abd4d8
graham <graham.sm94@gmail.com>
2016-06-12 15:11:17 -0400

    Configured request to send proper JSON data

--- front-end/src/components/App/SignUpForm/SignUp.jsx
+++ front-end/src/components/App/SignUpForm/SignUp.jsx
@@ -1,35 +1,45 @@
 import React, { Component } from 'react';
 import style from './sign_up_form.css'
+var Reqwest = require('reqwest');
 
 export default class SignUpForm extends React.Component{
   constructor(){
     super();
+    this.state = {};
   }
 
   handleSubmit(e){
     e.preventDefault();
-    fetch("http://localhost:3000/users",
-    {
-      method: "POST",
-      mode: 'cors',
-      header: new Headers(),
-      cache: 'default'
-    })
-    .then(function(response){
-      debugger;
-    })
-    .catch(function(response){
-      debugger;
+    this.setState({email: document.getElementById('email').value, first_name: document.getElementById('first-name').value, last_name: document.getElementById('last-name').value, password: document.getElementById('password').value},
+    function(){
+      Reqwest({
+        url: "http://localhost:3000/users",
+        type: 'json',
+        method: "post",
+        contentType: 'application/json',
+        header: new Headers(),
+        data: JSON.stringify(this.state),
+        success: function(response){
+          debugger;
+        },
+        error: function(response){
+          debugger;
+        }
+      })
     })
   }
 
   render(){
     return(
-      <div className = 'sign-up-form' onSubmit={this.handleSubmit}>
-      <form id="sign-up">
+      <div className = 'sign-up-form'>
+      <form id="sign-up" onSubmit={this.handleSubmit.bind(this)}>
         <div className="input">
-          <label htmlFor="full-name">Full Name</label>
-          <input type="text" id="full-name" name="full-name" autoCorrect="off" autoCapitalize="words" maxLength="32"></input>
+          <label htmlFor="full-name">First Name</label>
+          <input type="text" id="first-name" name="first-name" autoCorrect="off" autoCapitalize="words" maxLength="32"></input>
+        </div>
+        <div className="input">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" name="last-name" autoCorrect="off" autoCapitalize="words" maxLength="32"></input>
         </div>
         <div className="input">
           <label htmlFor="email">Email Address</label>
