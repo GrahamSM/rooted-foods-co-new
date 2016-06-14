aad2bd300530437dc74d067a74824cd3e519f587
graham <graham.sm94@gmail.com>
2016-06-14 17:58:07 -0400

    Added quantity and price fields

--- front-end/src/components/App/Cart/CartItem/CartItem.jsx
+++ front-end/src/components/App/Cart/CartItem/CartItem.jsx
@@ -14,6 +14,15 @@ export default class CartItem extends React.Component {
               <img src="http://image.desk7.net/Cate%20Wallpapers/7230_1280x800.jpg" height="100%" width="100%"></img>
             </div>
             <div className = 'cart-item-order'>
+              <div className = 'cart-item-description'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              </div>
+              <div className = "cart-item-quantity">
+                12
+              </div>
+              <div className = "cart-item-price">
+              $23.99
+              </div>
             </div>
             <div className = 'cart-item-total'>
             </div>
