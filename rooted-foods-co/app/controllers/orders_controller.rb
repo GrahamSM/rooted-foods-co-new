aca482edac5777bba5710b4e1527ead164f757b4
graham <graham.sm94@gmail.com>
2016-06-16 13:03:10 -0400

    Refactor orders_controller

--- rooted-foods-co/app/controllers/orders_controller.rb
+++ rooted-foods-co/app/controllers/orders_controller.rb
@@ -8,44 +8,17 @@ class OrdersController < ApplicationController
   def create
     current_order = current_user.orders.where(active: true, completed: false).first
     if (current_order) #an order is already in the process of being filled
-      bundle = Bundle.find(params[:id])
-      products = bundle.products
-      products.each do |product|
-        order_item = OrderItem.where(order_id: current_order.id, product_id: product.id).first
-        if (order_item)
-          order_item.quantity += params[:quantity]
-          order_item.save
-        else
-          current_order.products << product #there is no order item for this products and this order, so make it and set the quantity!
-          order_item = OrderItem.where(order_id: current_order.id, product_id: product.id).first
-          order_item.quantity = params[:quantity]
-          order_item.bundle = bundle
-          order_item.save
-        end
-      end
-      if current_order.save
-        render json: {}, status: 200
-      else
-        render json: {errors: current_order.errors.messages}, status: 422
-      end
+      current_order.update_products(id: params[:id], quantity: params[:quantity])
     else #create a new order
-      bundle = Bundle.find(params[:id])
-      products = bundle.products
       current_order = Order.create(order_params)
       current_order.user = current_user
       current_order.active = true
-      products.each do |product|
-        current_order.products << product
-        order_item = OrderItem.where(order_id: current_order.id, product_id: product.id).first
-        order_item.quantity = params[:quantity]
-        order_item.bundle = bundle
-        order_item.save
-      end
-      if current_order.save
-        render json: {}, status: 200
-      else
-        render json: {errors: current_order.errors.messages}, status: 422
-      end
+      current_order.update_products(id: params[:id], quantity: params[:quantity])
+    end
+    if current_order.save
+      render json: {}, status: 200
+    else
+      render json: {errors: current_order.errors.messages}, status: 422
     end
   end
 
--- rooted-foods-co/app/models/order.rb
+++ rooted-foods-co/app/models/order.rb
@@ -3,4 +3,23 @@ class Order < ActiveRecord::Base
   has_many :order_items
   has_many :bundles, through: :order_items
   has_many :products, through: :order_items
+
+
+  def update_products(options)
+    bundle = Bundle.find(options[:id])
+    bundle_products = bundle.products
+    bundle_products.each do |product|
+      order_item = OrderItem.where(order_id: self.id, product_id: product.id).first
+      if (order_item)
+        order_item.quantity += options[:quantity]
+        order_item.save
+      else
+        self.products << product
+        order_item = OrderItem.where(order_id: self.id, product_id: product.id).first
+        order_item.quantity = options[:quantity]
+        order_item.bundle = bundle
+        order_item.save
+      end
+    end
+  end
 end
