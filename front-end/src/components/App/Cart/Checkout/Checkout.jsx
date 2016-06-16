831127dd613b5f5cdaa0b0e9b1a13b251e9904f8
graham <graham.sm94@gmail.com>
2016-06-16 14:04:02 -0400

    Adjusted footer

--- front-end/src/components/App/Cart/Checkout/Checkout.jsx
+++ front-end/src/components/App/Cart/Checkout/Checkout.jsx
@@ -10,9 +10,24 @@ export default class Checkout extends React.Component {
     render() {
         return (
           <div className = 'checkout-item-wrapper'>
-              <div className = 'checkout-item-total'>
-                {this.props.total}
+            <div className='checkout-box'>
+              <div className='item subtotal'>
+                <label>Subtotal</label>
+                <div className='value'>10</div>
               </div>
+              <div className='item tax'>
+                <label>Subtotal</label>
+                <div className='value'>10</div>
+              </div>
+              <div className='item discount'>
+                <label>Subtotal</label>
+                <div className='value'>10</div>
+              </div>
+              <div className='item total'>
+                <label>Subtotal</label>
+                <div className='value'>10</div>
+              </div>
+            </div>
           </div>
         );
     }
