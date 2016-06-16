c5bf5d5f744ec51d436e7f48af13665cf429fd08
graham <graham.sm94@gmail.com>
2016-06-16 14:03:48 -0400

    Adjusted footer

--- rooted-foods-co/app/controllers/order_items_controller.rb
+++ rooted-foods-co/app/controllers/order_items_controller.rb
@@ -3,7 +3,6 @@ class OrderItemsController < ApplicationController
   def index
     order = current_user.orders.where(active: true, completed: false).first
     order_items = order.order_items
-    byebug
     render json: order_items.to_json(:include => [:product])
   end
 end
