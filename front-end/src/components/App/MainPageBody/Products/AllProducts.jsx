1464bf49ddcf49414f94b5074d4fcdab4dd2f741
graham <graham.sm94@gmail.com>
2016-06-20 13:39:31 -0400

    Added jquery to style active category filter buttons

--- front-end/src/components/App/MainPageBody/Products/AllProducts.jsx
+++ front-end/src/components/App/MainPageBody/Products/AllProducts.jsx
@@ -11,6 +11,21 @@ export default class AllProducts extends React.Component {
       this.state = {loading: true, count: 0, filter_by: null}
     }
 
+    render() {
+      return (
+        <div className="products-container">
+          <div className="products-filter">
+            <ProductsFilter className='category-filter meat' title="Meat" category_id="1" filterProducts={this._filterProducts} />
+            <ProductsFilter className='category-filter produce' title="Produce" category_id="2" filterProducts={this._filterProducts} />
+            <ProductsFilter className='category-filter grains' title="Grains" category_id="3" filterProducts={this._filterProducts} />
+          </div>
+          <div className="list">
+            {!this.state.loading && this.displayAllProducts()}
+          </div>
+        </div>
+      );
+    }
+
     componentWillMount = () =>{
       Reqwest({
           url: "http://localhost:3000/products",
@@ -25,21 +40,6 @@ export default class AllProducts extends React.Component {
       }).catch((error) => {
           alert(error.message);
       })
-    }
-
-    render() {
-      return (
-        <div className="products-container">
-          <div className="products-filter">
-            <ProductsFilter className='category-filter meat' title="Meat" category_id="1" filterProducts={this._filterProducts} />
-            <ProductsFilter className='category-filter produce' title="Produce" category_id="2" filterProducts={this._filterProducts} />
-            <ProductsFilter className='category-filter grains' title="Grains" category_id="3" filterProducts={this._filterProducts} />
-          </div>
-          <div className="list">
-            {!this.state.loading && this.displayAllProducts()}
-          </div>
-        </div>
-      );
     }
 
     displayAllProducts = () =>{
@@ -57,8 +57,17 @@ export default class AllProducts extends React.Component {
     _filterProducts = (category_id) =>{
       if (this.state.filter_by === parseInt(category_id)){
         this.setState({filter_by: null}) //Display all products
+        $('.category-filter').css('background-color', 'white');
       }else{
+        $('.category-filter').css('background-color', 'white');
         this.setState({filter_by: parseInt(category_id)}) //Filter by the passed in category_id
+        if (category_id === "1"){
+          $('.meat').css('background-color', '#c0392b');
+        }else if (category_id === "2"){
+          $('.produce').css('background-color', '#27ae60');
+        }else{
+          $('.grains').css('background-color', '#E87E04');
+        }
       }
     }
 }
--- front-end/src/components/App/MainPageBody/Products/ProductsFilter.jsx
+++ front-end/src/components/App/MainPageBody/Products/ProductsFilter.jsx
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import styles from './products_filter.scss';
 
 export default class ProductsFilter extends React.Component {
-    constructor() {
-      super();
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-      return (
-          <button className={this.props.className} onClick={ () => { this.props.filterProducts(this.props.category_id)}}>{this.props.title}</button>
-      );
-    }
+  render() {
+    return (
+        <button className={this.props.className} onClick={ () => { this.props.filterProducts(this.props.category_id)}}>{this.props.title}</button>
+    );
+  }
 }
