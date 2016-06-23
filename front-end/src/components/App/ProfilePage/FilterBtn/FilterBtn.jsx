b8f7c9ccfd013a2208c95b6248a0cd2f0a8f21b5
graham <graham.sm94@gmail.com>
2016-06-22 20:43:56 -0400

    Added filter buttons

---
+++ front-end/src/components/App/ProfilePage/FilterBtn/FilterBtn.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import styles from './filter_btn.scss';
+export default class FilterBtn extends React.Component{
+
+  render(){
+    return(
+      <button className={this.props.className}>{this.props.label}</button>
+    )
+  }
+}
--- front-end/src/components/App/ProfilePage/ProfilePage.jsx
+++ front-end/src/components/App/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import styles from './profile_page.scss';
+import FilterBtn from './FilterBtn/FilterBtn.jsx'
 export default class ProfilePage extends React.Component{
 
   render(){
+    // TODO: Give each FilterBtn a click handler that renders relevant data
     return(
-      <div>
+      <div className='wrapper'>
+        <div className='filter-btn-container'>
+          <FilterBtn className="test-btn" label="Favourites"/>
+          <FilterBtn className="test-btn" label="Order History"/>
+          <FilterBtn className="test-btn" label="Personalization"/>
+        </div>
+        <div className='account-page-container'>
+          <div className='account-page-col-1'>
+          </div>
+          <div className='account-page-col-2'>
+          </div>
+        </div>
       </div>
     )
   }
